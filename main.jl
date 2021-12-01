556b197459c43460243ab0c36b828546ed6c68e1
Rolf Kreibaum <rolf.kreibaum@posteo.de>
2021-12-01 17:38:54 +0000

    refactor day 1

--- main.jl
+++ main.jl
@@ -1,31 +1,27 @@
-# Day one
+## Advent of Code 2021 ##
+#########################
 
-d1_test = [199,200,208,210,200,207,240,269,260,263]
-day_1_input = parse.(Int, readlines(open("input-01")))
+## Day 01: Sonar Sweep ##
+#########################
 
-function depth_count(depth_array)
+day1_test = [199, 200, 208, 210, 200, 207, 240, 269, 260, 263]
+day1_input = parse.(Int, readlines(open("input-01")))
+
+"""Given a Vector of Int depth values, this counts how often it decreases when
+going from position i to i+step_size."""
+function depth_count(depth_array::Vector{Int}, step_size::Int = 1)
     counter = 0
-    for i in 1:(length(depth_array)-1)
-        if depth_array[i] < depth_array[i+1]
+    for i = 1:(length(depth_array)-step_size)
+        if depth_array[i] < depth_array[i+step_size]
             counter = counter + 1
         end
     end
     counter
 end
 
-@assert 7 == @show depth_count(d1_test)
-@assert 1532 == @show depth_count(day_1_input)
+@assert 0 == depth_count(day1_test, 100)
+@assert 7 == @show depth_count(day1_test)
+@assert 1532 == @show depth_count(day1_input)
 
-
-function sliding_window_increase(depth_array)
-    counter = 0
-    for i in 1:(length(depth_array)-3)
-        if depth_array[i] < depth_array[i+3]
-            counter = counter + 1
-        end
-    end
-    counter
-end
-
-@assert 5 == @show sliding_window_increase(d1_test)
-@assert 1532 == @show sliding_window_increase(day_1_input)
+@assert 5 == @show depth_count(day1_test, 3)
+@assert 1571 == @show depth_count(day1_input, 3)
